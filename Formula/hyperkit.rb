94e242df1a1692b1fa2beaa12fdab400e9419b35
Brandon-Barajas <barabra0743@gmail.com>
2018-07-30 16:13:22 -0600

    Merged in alepee's PR from homebrew-core
    Adds qcow support back in, should work across the board.
    Credit to [alepee](https://github.com/alepee)
    Pr for reference: [#25593](https://github.com/Homebrew/homebrew-core/pull/25593)

--- Formula/hyperkit.rb
+++ Formula/hyperkit.rb
@@ -3,15 +3,39 @@ class Hyperkit < Formula
   homepage "https://github.com/moby/hyperkit"
   url "https://github.com/moby/hyperkit/archive/v0.20180403.tar.gz"
   sha256 "e2739b034f20d9437696de48ace42600f55b7213292ec255032b2ef55f508297"
-  head "https://github.com/moby/hyperkit.git"
 
   resource "tinycorelinux" do
     url "https://dl.bintray.com/markeissler/homebrew/hyperkit-kernel/tinycorelinux_8.x.tar.gz"
     sha256 "560c1d2d3a0f12f9b1200eec57ca5c1d107cf4823d3880e09505fcd9cd39141a"
   end
 
+  depends_on "ocaml" => :build
+  depends_on "opam" => :build
+  depends_on :xcode => ["9.0", :build]
+  depends_on "libev"
+
   def install
-    system "make"
+    system "opam", "init", "--no-setup"
+    opam_dir = "#{buildpath}/.brew_home/.opam"
+    ENV["CAML_LD_LIBRARY_PATH"] = "#{opam_dir}/system/lib/stublibs:/usr/local/lib/ocaml/stublibs"
+    ENV["OPAMUTF8MSGS"] = "1"
+    ENV["PERL5LIB"] = "#{opam_dir}/system/lib/perl5"
+    ENV["OCAML_TOPLEVEL_PATH"] = "#{opam_dir}/system/lib/toplevel"
+    ENV.prepend_path "PATH", "#{opam_dir}/system/bin"
+
+    inreplace "#{opam_dir}/compilers/4.05.0/4.05.0/4.05.0.comp",
+      '["./configure"', '["./configure" "-no-graph"' # Avoid X11
+
+    ENV.deparallelize { system "opam", "switch", "4.05.0" }
+
+    system "opam", "config", "exec", "--",
+           "opam", "install", "-y", "uri", "qcow", "conduit.1.0.0", "lwt.3.1.0",
+           "qcow-tool", "mirage-block-unix.2.9.0", "conf-libev", "logs", "fmt",
+           "mirage-unix", "prometheus-app"
+
+    args = []
+    args << "GIT_VERSION=#{version}"
+    system "opam", "config", "exec", "--", "make", *args
 
     bin.install "build/hyperkit"
     man1.install "hyperkit.1"
